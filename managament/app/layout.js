e14ef69a33e8dc0a3d7a52f0b4b5e68ae8188094
Jeevitha-Ravikumar27 <ravikumarjeevitha254@gmail.com>
2024-12-28 19:39:25 +0530

    m

---
+++ managament/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Zcrum",
+  description: "Project Management app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ managament/app/page.js
@@ -0,0 +1,12 @@
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (<div>
+    <Button variant="default" size="default">Go</Button>
+     Hi to Coding
+  </div>
+  );
+  
+
+}
